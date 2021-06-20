f1d46ab2806253cc1a0511470192f282771c5f70
Michael Lyon <mlyon360@gmail.com>
2021-06-20 14:53:17 -0600

    add eslint/prettier and tailwind/twin.macro

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,13 @@
-import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import GlobalStyles from 'components/GlobalStyles'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+const MyApp = ({ Component, pageProps }: AppProps) => {
+  return (
+    <>
+      <GlobalStyles />
+      <Component {...pageProps} />
+    </>
+  )
 }
+
 export default MyApp
