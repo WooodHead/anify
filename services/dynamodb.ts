da37a0b32543b45511f684c3ada023c01a2979bd
Michael Lyon <mlyon360@gmail.com>
2021-07-11 12:28:07 -0600

    update env variable names

--- services/dynamodb.ts
+++ services/dynamodb.ts
@@ -9,9 +9,9 @@ import { DataSource } from 'apollo-datasource'
 type AnimeTableAttributes = { id: string; entity: string }
 
 dynamoose.aws.sdk.config.update({
-  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-  region: process.env.AWS_REGION,
+  accessKeyId: process.env.ACCESS_KEY_ID,
+  secretAccessKey: process.env.SECRET_ACCESS_KEY,
+  region: process.env.REGION,
 })
 export class AnimeEntity extends Document {
   PK!: AnimeTableAttributes
