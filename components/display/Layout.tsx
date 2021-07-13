739bc115a76ce62532e94670bcb3a4ff01fa8a46
Isaiah Fielder <isaiahmfielder@gmail.com>
2021-07-13 16:27:49 -0600

    remove scrollbar from layout

--- components/display/Layout.tsx
+++ components/display/Layout.tsx
@@ -47,8 +47,6 @@ const Layout = ({
               colorMode === 'dark' ? 'os-theme-light' : 'os-theme-dark'
             }
             $noPadding={noPadding}
-            options={{ scrollbars: { autoHide: 'scroll' } }}
-            ref={scrollBarRef}
           >
             {/* give all children access to the scrollBarRef just incase 🤗 */}
             {React.Children.map(children, (child) => {
@@ -74,6 +72,6 @@ const ContentContainer = tw.div`relative flex flex-grow`
 const Content = styled(OverlayScrollbarsComponent)<{ $noPadding: boolean }>`
   ${({ $noPadding }) => [
     tw`relative h-full flex-grow bg-gray-50 dark:bg-gray-900 transition-colors overflow-hidden w-screen`,
-    !$noPadding && tw`px-6 md:px-14 py-8 md:py-10`,
+    !$noPadding && tw`px-6 md:px-14 py-8 md:py-10 overflow-y-auto`,
   ]}
 `
